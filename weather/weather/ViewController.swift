10cb7e3cbcebb0557c4a24d48afbb6cbe23efc83
voitivit <kurbanov_emil@icloud.com>
2023-09-26 08:37:51 +0300

    project added

---
+++ weather/weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by emil kurbanov on 26.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
